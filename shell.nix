61c420e962eae829d7f89c8d0d4fe69a9e767bfe
Niklaus Giger <niklaus.giger@member.fsf.org>
2022-01-25 11:43:09 +0100

    Added skel

---
+++ shell.nix
@@ -0,0 +1,6 @@
+# Helper script for Niklaus to be able to work under NixOS
+# To get into a fish shell call in this directory: nix-shell --command fish
+{ pkgs ? import <nixpkgs> {} }:
+ pkgs.mkShell {
+  buildInputs = [ pkgs.zlib pkgs.git pkgs.bundler pkgs.ruby ];
+}
