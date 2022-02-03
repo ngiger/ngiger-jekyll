1658014c0d51a8d123754dcd40b4199f69608352
Niklaus Giger <niklaus.giger@member.fsf.org>
2022-02-03 15:39:37 +0100

    Added a better shell.nix

--- blog/shell.nix
+++ blog/shell.nix
@@ -1,7 +1,6 @@
-# Helper script for Niklaus to be able to work under NixOS
-# To get into a fish shell call in this directory: nix-shell --command fish
-{ pkgs ? import <nixpkgs> {} }:
- pkgs.mkShell {
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "env";
   buildInputs = [ pkgs.zlib pkgs.git pkgs.bundler pkgs.ruby.devEnv pkgs.bundix
   ];
 }
